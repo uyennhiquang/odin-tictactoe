c0cef0c7d90052c1d5b314c1f13faf7567e85e07
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2023-06-28 12:12:54 -0400

    Add initial game logic The main game script uses a Gameboard module that contains the game logic, and player-related objects/variables.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,114 @@
-const gameboard = [];
-const player = () => {
-  return { };
-}
+"use strict";
+
+const boardSize = 3;
+
+// Game Logic
+const Gameboard = (() => {
+  // Players
+  const PlayerOne = (name) => {
+    return { name, mark: "O" };
+  };
+  const PlayerTwo = (name) => {
+    return { name, mark: "X" };
+  };
+
+  const p1 = PlayerOne("jane");
+  const p2 = PlayerTwo("tim");
+
+  // Gameboard shenanigans
+  const gameboard = (() => {
+    const arr = [];
+
+    for (let i = 0; i < boardSize; i++) {
+      const temp = [];
+      for (let j = 0; j < boardSize; j++) {
+        temp.push("");
+      }
+      arr.push(temp);
+    }
+
+    return arr;
+  })();
+
+  const addMark = (mark, location) => {
+    // Ex. of location: [1, 2]
+    const [row, col] = location;
+    if (!gameboard[row][col]) gameboard[row][col] = mark;
+  };
+  
+  const isGameOver = () => {
+    // Check for straights
+    for (let i = 0; i < boardSize; i++) {
+      for (let j = 0; j < boardSize; j++) {
+        
+      }
+    }
+
+    // Check for diagonals
+
+    // Check for tie
+  }
+
+  return {
+    gameboard,
+    addMark,
+    p1,
+    p2,
+  };
+})();
+
+const indexTable = (() => {
+  const tbodyEl = document.getElementById("gameboard-table").firstElementChild;
+  console.log(tbodyEl);
+  for (let i = 0; i < boardSize; i++) {
+    const currentRow = tbodyEl.children[i];
+    currentRow.setAttribute("data-row", i);
+    for (let j = 0; j < boardSize; j++) {
+      const currentTD = currentRow.children[j];
+      currentTD.setAttribute("data-col", j);
+    }
+  }
+})();
+
+// the real DOM shit
+const startBtn = document.getElementById("button--start");
+let isGame = true;
+
+startBtn.addEventListener("click", function () {
+  if (isGame) {
+    const boxes = document.querySelectorAll("td");
+    const p1Indicator = document.getElementById("indicator--p1");
+    const p2Indicator = document.getElementById("indicator--p2");
+
+    let isGame = false;
+    let activePlayer = 1;
+
+    const init = (() => {
+      activePlayer = 1;
+      p1Indicator.classList.toggle("player--active");
+    })();
+
+    const switchPlayer = function () {
+      activePlayer = activePlayer == 1 ? 2 : 1;
+      p1Indicator.classList.toggle("player--active");
+      p2Indicator.classList.toggle("player--active");
+    };
+
+    for (let i = 0; i < boxes.length; i++) {
+      boxes[i].addEventListener("click", function (event) {
+        const row = event.target.closest("tr").getAttribute("data-row");
+        const col = boxes[i].getAttribute("data-col");
+        if (!boxes[i].innerHTML) {
+          if (activePlayer === 1) {
+            Gameboard.addMark(Gameboard.p1.mark, [row, col]);
+            boxes[i].innerHTML = Gameboard.p1.mark;
+          } else if (activePlayer === 2) {
+            Gameboard.addMark(Gameboard.p2.mark, [row, col]);
+            boxes[i].innerHTML = Gameboard.p2.mark;
+          }
+          switchPlayer();
+        }
+      });
+    }
+  }
+});
