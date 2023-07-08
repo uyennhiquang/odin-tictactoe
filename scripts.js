3d1d24b1c39d972787d0c0bbe0b58da8592ec00d
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2023-07-08 11:35:31 -0400

    Give default value to congratulating message

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ const boardSize = 3;
 
 // Game Logic
 const Gameboard = (() => {
-  // Players
+  // Gameboard properties
   const PlayerOne = (name) => {
     return { name, mark: "O" };
   };
@@ -12,10 +12,9 @@ const Gameboard = (() => {
     return { name, mark: "X" };
   };
 
-  const p1 = PlayerOne("jane");
-  const p2 = PlayerTwo("tim");
+  const p1 = PlayerOne("");
+  const p2 = PlayerTwo("");
 
-  // Gameboard shenanigans
   const gameboard = (() => {
     const arr = [];
 
@@ -30,6 +29,8 @@ const Gameboard = (() => {
     return arr;
   })();
 
+  let gameStatus = false;
+  // Gameboard methods
   const addMark = (mark, location) => {
     // Ex. of location: [1, 2]
     const [row, col] = location;
@@ -37,7 +38,8 @@ const Gameboard = (() => {
   };
 
   const isGameOver = () => {
-    // The logic below will either return a winner or 'tie'
+    // The logic below will either return 'X'/'O' or 'tie'.
+    // The if (output) makes sure the output of the checking isn't an empty string.
 
     // Check for across
     for (let i = 0; i < boardSize; i++) {
@@ -49,12 +51,14 @@ const Gameboard = (() => {
         if (output) return output;
       }
     }
+
     // Check for straights
     for (let i = 0; i < boardSize; i++) {
       if (
         gameboard[0][i] === gameboard[1][i] &&
         gameboard[1][i] === gameboard[2][i]
       ) {
+        // console.log(gameStatus);
         const output = gameboard[0][i];
         if (output) return output;
       }
@@ -73,24 +77,39 @@ const Gameboard = (() => {
 
     // Check for tie
     // let tie = false;
+
+    // TBD: tie logic + DOM indicator
     for (let i = 0; i < boardSize; i++) {
       if (gameboard[i].includes("")) return 1;
     }
 
-    // return gameStatus;
     return "tie";
   };
 
+  const clearGameboard = () => {
+    for (let i = 0; i < boardSize; i++) {
+      for (let j = 0; j < boardSize; j++) {
+        gameboard[i][j] = "";
+      }
+    }
+  };
+
   return {
-    gameboard,
     addMark,
     isGameOver,
+    clearGameboard,
+    gameboard,
+    gameStatus,
     p1,
     p2,
   };
 })();
 
-const indexTable = (() => {
+// the real DOM shit
+
+// Initialization
+(() => {
+  // Indexing table
   const tbodyEl = document.getElementById("gameboard-table").firstElementChild;
   for (let i = 0; i < boardSize; i++) {
     const currentRow = tbodyEl.children[i];
@@ -100,24 +119,32 @@ const indexTable = (() => {
       currentTD.setAttribute("data-col", j);
     }
   }
+
+  // Clear name inputs
+  const inputEl = document.querySelectorAll("input");
+  for (let i = 0; i < inputEl.length; i++) inputEl[i].value = "";
 })();
 
-// the real DOM shit
-const startBtn = document.getElementById("button--start");
-let isGame = true;
+const GameboardDOM = (() => {
+  let gameStatus = false;
 
-startBtn.addEventListener("click", function () {
-  if (isGame) {
-    const boxes = document.querySelectorAll("td");
-    const p1Indicator = document.getElementById("indicator--p1");
-    const p2Indicator = document.getElementById("indicator--p2");
-    const winnerIndicator = document.getElementById("indicator--winner");
+  const boxes = document.querySelectorAll("td");
+  const p1Indicator = document.getElementById("indicator--p1");
+  const p2Indicator = document.getElementById("indicator--p2");
+  const winnerIndicator = document.getElementById("indicator--winner");
 
-    let isGame = false;
+  const gameStart = function () {
+    gameStatus = true;
+
+    Gameboard.p1.name = p1Indicator.querySelector("input").value;
+    Gameboard.p2.name = p2Indicator.querySelector("input").value;
+
     let activePlayer = 1;
-
-    const init = (() => {
+    const initPlayerSetting = (() => {
       activePlayer = 1;
+      p1Indicator.classList.remove("player--active");
+      p2Indicator.classList.remove("player--active");
+
       p1Indicator.classList.toggle("player--active");
     })();
 
@@ -129,25 +156,59 @@ startBtn.addEventListener("click", function () {
 
     for (let i = 0; i < boxes.length; i++) {
       boxes[i].addEventListener("click", function (event) {
-        const row = event.target.closest("tr").getAttribute("data-row");
-        const col = boxes[i].getAttribute("data-col");
+        if (gameStatus) {
+          const row = event.target.closest("tr").getAttribute("data-row");
+          const col = boxes[i].getAttribute("data-col");
 
-        if (!boxes[i].innerHTML) {
-          if (activePlayer === 1) {
-            Gameboard.addMark(Gameboard.p1.mark, [row, col]);
-            boxes[i].innerHTML = Gameboard.p1.mark;
-          } else if (activePlayer === 2) {
-            Gameboard.addMark(Gameboard.p2.mark, [row, col]);
-            boxes[i].innerHTML = Gameboard.p2.mark;
+          if (!boxes[i].innerHTML) {
+            if (activePlayer === 1) {
+              Gameboard.addMark(Gameboard.p1.mark, [row, col]);
+              boxes[i].innerHTML = Gameboard.p1.mark;
+            } else if (activePlayer === 2) {
+              Gameboard.addMark(Gameboard.p2.mark, [row, col]);
+              boxes[i].innerHTML = Gameboard.p2.mark;
+            }
+
+            if (Gameboard.isGameOver() === Gameboard.p1.mark) {
+              winnerIndicator.innerHTML = `Congrats, ${
+                Gameboard.p1.name || "Player 1"
+              }!`;
+              gameStatus = false;
+            } else if (Gameboard.isGameOver() === Gameboard.p2.mark) {
+              winnerIndicator.innerHTML = `Congrats, ${
+                Gameboard.p2.name || "Player 2"
+              }!`;
+              gameStatus = false;
+            }
+            switchPlayer();
           }
-          
-          if (Gameboard.isGameOver() === Gameboard.p1.mark)
-            winnerIndicator.innerHTML = `Congrats, ${Gameboard.p1.name}!`;
-          else if (Gameboard.isGameOver() === Gameboard.p2.mark)
-            winnerIndicator.innerHTML = `Congrats, ${Gameboard.p2.name}!`;
-          switchPlayer();
         }
       });
     }
-  }
+  };
+
+  const clearGameboard = function () {
+    for (let i = 0; i < boxes.length; i++) {
+      boxes[i].innerHTML = "";
+    }
+  };
+
+  return {
+    gameStart,
+    clearGameboard,
+  };
+})();
+
+const startBtn = document.getElementById("button--start");
+const restartBtn = document.getElementById("button--restart");
+
+restartBtn.addEventListener("click", function () {
+  Gameboard.clearGameboard();
+
+  GameboardDOM.clearGameboard();
+  GameboardDOM.gameStart();
+});
+
+startBtn.addEventListener("click", function () {
+  GameboardDOM.gameStart();
 });
